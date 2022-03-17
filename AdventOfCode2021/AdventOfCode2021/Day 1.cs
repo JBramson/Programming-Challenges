01ebc3214bc75f551488cdbe3f2ab7a4ebac6e83
JBramson <jackbramson@gmail.com>
2022-03-16 18:00:56 -0700

    Started 2021 AoC with C# through Rider.

---
+++ AdventOfCode2021/AdventOfCode2021/Day 1.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * https://adventofcode.com/2021/day/1
+ * Objective: Given a list of ints, count the number of values greater than their predecessor.
+ * Part of me learning C#.
+ */
+
+
+IEnumerable<string> numberStrings = System.IO.File.ReadLines("/home/jack/Dev/Programming-Challenges/AdventOfCode2021/AdventOfCode2021/input.txt");
+List<int> depths = new List<int>();
+int increases = 0;
+
+// Cast strings to ints
+foreach (var numberStr in numberStrings)
+{
+	depths.Add(Convert.ToInt16(numberStr));
+}
+// Check vals
+for (int i = 1; i < depths.Count; i++)
+{
+	if (depths[i] > depths[i - 1])
+	{
+		increases++;
+	}
+}
+
+Console.WriteLine(increases);
