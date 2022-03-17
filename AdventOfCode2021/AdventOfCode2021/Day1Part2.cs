f5cdbca642cc47d779ff3d09f7c5796252c0f733
JBramson <jackbramson@gmail.com>
2022-03-16 18:44:48 -0700

    Finished second part of day 1 and fixed .gitignore.

---
+++ AdventOfCode2021/AdventOfCode2021/Day1Part2.cs
@@ -0,0 +1,41 @@
+/*
+ * https://adventofcode.com/2021/day/1#part2
+ * Objective: Given a list of ints, count the number of three successive values summed
+ * that are greater than their predecessor.
+ * Part of me learning C#.
+ */
+namespace AdventOfCode2021;
+
+public class Day_2
+{
+	static void Main(string[] args)
+	{
+		IEnumerable<string> numberStrings = System.IO.File.ReadLines("/home/jack/Dev/Programming-Challenges/AdventOfCode2021/AdventOfCode2021/input.txt");
+		List<int> depths = new List<int>();
+		List<int> depth_trips = new List<int>();
+		int increases = 0;
+    
+		// Cast strings to ints
+		foreach (var numberStr in numberStrings)
+		{
+			depths.Add(Convert.ToInt16(numberStr));
+		}
+
+		// Group by threes
+		for (int i = 0; i < depths.Count - 2; i++)
+		{
+			depth_trips.Add(depths[i] + depths[i + 1] + depths[i + 2]);
+		}
+		
+		// Check vals
+		for (int i = 1; i < depth_trips.Count; i++)
+		{
+			if (depth_trips[i] > depth_trips[i - 1])
+			{
+				increases++;
+			}
+		}
+		
+		Console.WriteLine(increases);
+	}
+}
