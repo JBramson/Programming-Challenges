c627ea5a068f1fda7f036e18f674bf25105ac0ec
JBramson <jackbramson@gmail.com>
2022-04-05 23:53:02 -0700

    Started working on part 2- will need some trick to make it go faster.

--- AdventOfCode2021/AdventOfCode2021/Day6.cs
+++ AdventOfCode2021/AdventOfCode2021/Day6.cs
@@ -1,8 +1,8 @@
 /*
  * https://adventofcode.com/2021/day/6
  * Objective: Given an initial state of lanternfish reproduction timers, track their reproduction over time.
- * Part 1: 
- * Part 2: 
+ * Part 1: How many will there be after 80 days?
+ * Part 2: How many will there be after 256 days?
  * Part of me learning C#.
  */
 
@@ -10,17 +10,47 @@ namespace AdventOfCode2021;
 
 public class Day6
 {
+	static List<Int16> timers = new List<short>();
+	
+	static void AddNewFish(int numToAdd)
+	{
+		for (int i = 0; i < numToAdd; i++)
+		{
+			timers.Add(8);
+		}
+	}
 	static void Main()
 	{
 		string initialLineStr =
 			File.ReadAllText("/home/jack/Dev/Programming-Challenges/AdventOfCode2021/AdventOfCode2021/input.txt");
-		List<Int16> timers = new List<short>();
+		// Change numberOfDays to test each part.
+		const int numberOfDays = 256;
+		int currentReproductionCount = 0;
 
 		foreach (string initialNumberStr in initialLineStr.Split(","))
 		{
 			timers.Add(Convert.ToInt16(initialNumberStr));
 		}
+
+		for (int i = 0; i < numberOfDays; i++)
+		{
+			for (int j = 0; j < timers.Count; j++)
+			{
+				if (timers[j] == 0)
+				{
+					currentReproductionCount++;
+					timers[j] = 6;
+				}
+				else
+				{
+					timers[j]--;
+				}
+			}
+			AddNewFish(currentReproductionCount);
+			currentReproductionCount = 0;
+			Console.WriteLine(i);
+		}
 		
-		Console.WriteLine(timers);
+		Console.WriteLine(timers.Count);
 	}
 }
