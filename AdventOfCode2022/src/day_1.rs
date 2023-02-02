e75bd885b5af7e8800c1cd53849988c26173cc09
Jack Bramson <jackbramson@gmail.com>
2023-02-01 22:18:14 -0800

    Added AOC boilerplate.

---
+++ AdventOfCode2022/src/day_1.rs
@@ -0,0 +1,17 @@
+/*
+ * https://adventofcode.com/2022/day/1
+ * Objective: Given a list of calories carried by elves,
+ * Part 1: Find the greatest total of calories carried by an elf.
+ * Part 2:
+ * Part of me learning Rust.
+ */
+
+pub fn part_1_solution(input_strings: Vec<String>) {
+    for line in input_strings {
+        println!("{}", line);
+    }
+}
+
+pub fn part_2_solution(_input_strings: Vec<String>) {
+
+}
