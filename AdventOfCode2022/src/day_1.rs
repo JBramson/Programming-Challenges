82be8a869d455cc617a8546e17b8360ad5ebccbe
Jack Bramson <jackbramson@gmail.com>
2023-02-03 22:07:12 -0800

    Solved day 1.

--- AdventOfCode2022/src/day_1.rs
+++ AdventOfCode2022/src/day_1.rs
@@ -6,12 +6,47 @@
  * Part of me learning Rust.
  */
 
-pub fn part_1_solution(input_strings: Vec<String>) {
+/// Manual looping and checking solution
+pub fn part_1_solution(input_strings: Vec<String>) -> i32 {
+    let mut max_calorie_count = 0;
+    let mut current_calorie_count = 0;
+
     for line in input_strings {
-        println!("{}", line);
+        if line == "" {
+            if current_calorie_count > max_calorie_count {
+                max_calorie_count = current_calorie_count;
+            }
+            current_calorie_count = 0;
+        } else {
+            current_calorie_count += str::parse::<i32>(&*line).unwrap();
+        }
     }
+    // Last input line shouldn't be blank, so we need to check the final elf
+    if current_calorie_count > max_calorie_count {
+        max_calorie_count = current_calorie_count;
+    }
+
+    max_calorie_count
 }
 
-pub fn part_2_solution(_input_strings: Vec<String>) {
+/// Smarter attempt with standard sort
+pub fn part_2_solution(input_strings: Vec<String>) -> i32 {
+    let mut calorie_counts: Vec<i32> = Vec::new();
+    let mut current_calorie_count = 0;
 
+    for line in input_strings {
+        if line == "" {
+            calorie_counts.push(current_calorie_count);
+            current_calorie_count = 0;
+        } else {
+            current_calorie_count += str::parse::<i32>(&*line).unwrap();
+        }
+    }
+    // Last input line shouldn't be blank, so we need to insert the final elf
+    calorie_counts.push(current_calorie_count);
+    calorie_counts.sort();
+
+    calorie_counts[calorie_counts.len() - 1]
+        + calorie_counts[calorie_counts.len() - 2]
+        + calorie_counts[calorie_counts.len() - 3]
 }
