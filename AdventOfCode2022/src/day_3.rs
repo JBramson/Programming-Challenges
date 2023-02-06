9e1f9397ff76d10c85d9158683c249339ef96094
Jack Bramson <jackbramson@gmail.com>
2023-02-06 13:50:45 -0800

    Finished day 3 part 1.

--- AdventOfCode2022/src/day_3.rs
+++ AdventOfCode2022/src/day_3.rs
@@ -6,6 +6,10 @@
  * Part of me learning Rust.
  */
 
+const LOWERCASE_MIN:  i32 = 97; // 'a' as i32
+const UPPERCASE_MIN:  i32 = 65; // 'A' as i32
+const CAPITAL_OFFSET: i32 = 26;
+
 fn find_common_item(left_compartment: &str, right_compartment: &str) -> char {
     for left_item in left_compartment.chars() {
         for right_item in right_compartment.chars() {
@@ -19,18 +23,23 @@ fn find_common_item(left_compartment: &str, right_compartment: &str) -> char {
 }
 
 fn get_item_priority(item: char) -> i32 {
-    -1
+    let item_i32 = item as i32;
+
+    return if item_i32 >= LOWERCASE_MIN {
+        item_i32 - LOWERCASE_MIN + 1
+    } else {
+        item_i32 - UPPERCASE_MIN + 1 + CAPITAL_OFFSET
+    }
 }
 
 pub fn part_1_solution(input_strings: Vec<String>) -> i32 {
-    let mut priority_total = 0;
+    let mut priority_total: i32 = 0;
 
     for line in input_strings {
         let compartment_size = line.len() / 2;
         let common_item = find_common_item(&line[..compartment_size],
                                            &line[compartment_size..]);
         priority_total += get_item_priority(common_item);
-        println!("{}", common_item);
     }
 
     priority_total
