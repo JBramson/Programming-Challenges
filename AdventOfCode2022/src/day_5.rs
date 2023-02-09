20d577af66889c9978568172f42b1af65c2f928b
Jack Bramson <jackbramson@gmail.com>
2023-02-08 21:58:35 -0800

    Started working on day 5 part 1.

---
+++ AdventOfCode2022/src/day_5.rs
@@ -0,0 +1,32 @@
+/*
+ * https://adventofcode.com/2022/day/2
+ * Objective: Given a stack of crates and a list of moves,
+ * Part 1: Perform the moves and return the top crates.
+ * Part 2:
+ * Part of me learning Rust.
+ */
+
+
+pub fn part_1_solution(input_strings: Vec<String>) -> &'static str {
+    let mut final_tops = "";
+    let mut stacks: [Vec<char>; 3] = [vec!['Z', 'N'], vec!['M', 'C', 'M'], vec!['P']];
+
+    println!("{:?}", stacks);
+
+    for line in input_strings {
+
+    }
+
+    final_tops
+}
+
+pub fn part_2_solution(input_strings: Vec<String>) -> &'static str {
+    let mut final_tops = "";
+    let mut stacks: [Vec<char>; 3] = [vec!['Z', 'N'], vec!['M', 'C', 'M'], vec!['P']];
+
+    for line in input_strings {
+
+    }
+
+    final_tops
+}
