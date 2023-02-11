3d58ce8eb8260eb17d9c729355c4a0c0cb3da9e9
Jack Bramson <jackbramson@gmail.com>
2023-02-10 21:25:08 -0800

    Assigned memory for vectors.

--- AdventOfCode2022/src/day_5.rs
+++ AdventOfCode2022/src/day_5.rs
@@ -6,15 +6,30 @@
  * Part of me learning Rust.
  */
 
+// The starting stack for the practice input:
+// let mut stacks: [Vec<char>; 3] = [vec!['Z', 'N'], vec!['M', 'C', 'M'], vec!['P']];
+const MINIMUM_NUMBER_OF_STACKS: usize = 3;
+const MAXIMUM_NUMBER_OF_STACKS: usize = 9;
 
 pub fn part_1_solution(input_strings: Vec<String>) -> &'static str {
     let mut final_tops = "";
-    let mut stacks: [Vec<char>; 3] = [vec!['Z', 'N'], vec!['M', 'C', 'M'], vec!['P']];
-
-    println!("{:?}", stacks);
+    let mut initial_stacks_lines: Vec<String> = vec![];
+    let mut stacks: Vec<Vec<char>> = vec![vec![]; MAXIMUM_NUMBER_OF_STACKS];
 
     for line in input_strings {
+        if line.contains('[') { // Stack lines
+            initial_stacks_lines.push(line);
+        } else if line.as_bytes()[1] == '1' as u8 { // Number line
+            let number_of_stacks = line.len() / 4;
+            if (number_of_stacks != MINIMUM_NUMBER_OF_STACKS)
+                && (number_of_stacks != MAXIMUM_NUMBER_OF_STACKS) {
+                panic!("Incorrect stack size generated: number_of_stacks={}", number_of_stacks);
+            }
+            todo!("Populate array vectors accordingly.");
 
+        } else if line.as_bytes()[0] == 'm' as u8 { // Movement lines
+            todo!("Register and make a move.");
+        }
     }
 
     final_tops
@@ -22,7 +37,6 @@ pub fn part_1_solution(input_strings: Vec<String>) -> &'static str {
 
 pub fn part_2_solution(input_strings: Vec<String>) -> &'static str {
     let mut final_tops = "";
-    let mut stacks: [Vec<char>; 3] = [vec!['Z', 'N'], vec!['M', 'C', 'M'], vec!['P']];
 
     for line in input_strings {
 
