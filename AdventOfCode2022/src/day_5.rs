24a1867333400bd15bcc47659fbb3a560f25dac5
Jack Bramson <jackbramson@gmail.com>
2023-06-02 11:18:54 -0400

    Finsihed day 5 part 1

--- AdventOfCode2022/src/day_5.rs
+++ AdventOfCode2022/src/day_5.rs
@@ -8,36 +8,36 @@
 // mod helpers;
 use crate::helpers::RunMode;
 
-// The base starting stack for the practice input:
-// let mut stacks: [Vec<char>; 3] = [vec!['Z', 'N'], vec!['M', 'C', 'M'], vec!['P']];
-const MINIMUM_NUMBER_OF_STACKS: usize = 3;
-const MAXIMUM_NUMBER_OF_STACKS: usize = 9;
+const MINIMUM_NUMBER_OF_STACKS:       usize = 3;
+const MAXIMUM_NUMBER_OF_STACKS:       usize = 9;
+const INPUT_CHAR_HORIZONTAL_DISTANCE: usize = 4;
 
-fn move_top_crates(stacks: &mut Vec<Vec<char>>, quantity: u8, origin_stack: u8, target_stack: u8) -> bool {
-    todo!("Implement moving function");
-
-    false
+fn move_top_crates(stacks: &mut Vec<Vec<char>>, quantity: u8, origin_stack: usize, target_stack: usize) {
+    for i in 0..quantity {
+        let popped_crate = stacks[origin_stack].pop().unwrap();
+        stacks[target_stack].push(popped_crate);
+    }
 }
 
-pub fn part_1_solution(input_strings: Vec<String>, run_mode: RunMode) -> &'static str {
-    let mut final_tops = "";
+pub fn part_1_solution(input_strings: Vec<String>, run_mode: RunMode) -> String {
+    let mut final_tops: String = String::from("");
     let stack_count = if matches!(run_mode, RunMode::DEBUG) {MINIMUM_NUMBER_OF_STACKS} else {MAXIMUM_NUMBER_OF_STACKS};
     let mut initial_stacks_lines: Vec<Vec<char>> = vec![];
     let mut stacks: Vec<Vec<char>> = vec![];
 
-    for line in input_strings {
-        if line.contains('[') { // Stack lines
+    for mut line in input_strings {
+        if line.len() == 0 {
+            continue; // We can't test against the blank line
+        } else if line.contains('[') { // Stack lines
             let mut filtered_line: Vec<char> = vec![' '; stack_count];
             for i in 0..line.len() {
                 if i % 4 == 1 {
-                    filtered_line[i / 4] = line.chars().collect::<Vec<char>>()[i]; // i / 4 yields ints
+                    filtered_line[i / INPUT_CHAR_HORIZONTAL_DISTANCE] = line.chars().collect::<Vec<char>>()[i];
                 }
             }
             initial_stacks_lines.push(filtered_line);
         } else if line.as_bytes()[1] == '1' as u8 { // Number line
-            // Top loop iterates 0..stack_count to get stack num
-            // For each stack num, top to bottom, adding to the back of the stack (pop later from the front)
-            // Insert the stack
+            // Rotate the stacks
             for i in 0..stack_count {
                 let mut new_stack: Vec<char> = vec![];
                 let mut next_char: char;
@@ -51,18 +51,25 @@ pub fn part_1_solution(input_strings: Vec<String>, run_mode: RunMode) -> &'stati
                 }
                 stacks.push(new_stack);
             }
-
-            println!("{:?}", stacks);
         } else if line.as_bytes()[0] == 'm' as u8 { // Movement lines
-            todo!("Register and make a move.");
+            line.retain(|c| c.is_digit(10) || c == ' '); // Remove letters from line
+            let move_instructions: Vec<&str> = line.split_whitespace().collect();
+            move_top_crates(&mut stacks, move_instructions[0].parse::<u8>().unwrap(),
+                            move_instructions[1].parse::<usize>().unwrap() - 1,
+                            move_instructions[2].parse::<usize>().unwrap() - 1);
         }
+    }
+
+    // Get the final tops
+    for i in 0..stack_count {
+        final_tops.push(*stacks[i].last().unwrap());
     }
 
     final_tops
 }
 
-pub fn part_2_solution(input_strings: Vec<String>, run_mode: RunMode) -> &'static str {
-    let mut final_tops = "";
+pub fn part_2_solution(input_strings: Vec<String>, run_mode: RunMode) -> String {
+    let mut final_tops = String::from("");
 
     for line in input_strings {
 
