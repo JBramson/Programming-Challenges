f11627b364920b0b1c3d691b77099fbd3a782c02
Jack Bramson <jackbramson@gmail.com>
2023-06-14 19:02:32 -0400

    Finished day 6 part 2

--- AdventOfCode2022/src/day_6.rs
+++ AdventOfCode2022/src/day_6.rs
@@ -2,12 +2,13 @@
  * https://adventofcode.com/2022/day/6
  * Objective: Given a character stream,
  * Part 1: Find the first character where it and the preceding 3 share no chars in common.
- * Part 2:
+ * Part 2: Find the first character where it and the preceding 13 share no chars in common.
  * Part of me learning Rust.
  */
 use crate::helpers::RunMode;
 
 const MAX_SLICE_SIZE_PART_1: usize = 4;
+const MAX_SLICE_SIZE_PART_2: usize = 14;
 
 fn find_last_repeated_char_location(string: &str, end_char_location: usize, max_slice_size: usize) -> Option<usize> {
     for end_offset in 0..(max_slice_size - 1) {
@@ -40,8 +41,15 @@ pub fn part_1_solution(input_strings: Vec<String>, run_mode: RunMode) -> String 
 }
 
 pub fn part_2_solution(input_strings: Vec<String>, run_mode: RunMode) -> String {
-    let mut final_tops: String = String::from("");
+    let master_string: &str = input_strings.last().unwrap();
 
+    let mut i = MAX_SLICE_SIZE_PART_2 - 1; // Check from the last character of the slice
+    while i < master_string.len() {
+        match find_last_repeated_char_location(master_string, i, MAX_SLICE_SIZE_PART_2) {
+            Some(x) => i = x + MAX_SLICE_SIZE_PART_2,
+            None => return (i + 1).to_string(),
+        }
+    }
 
-    final_tops
+    String::from("Didn't find a valid slice :(")
 }
