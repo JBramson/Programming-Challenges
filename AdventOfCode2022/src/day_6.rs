ec6af15eaa093b609b7646c711eb29bfddd273fb
Jack Bramson <jackbramson@gmail.com>
2023-06-14 18:59:47 -0400

    Finished day 6 part 1

--- AdventOfCode2022/src/day_6.rs
+++ AdventOfCode2022/src/day_6.rs
@@ -7,21 +7,16 @@
  */
 use crate::helpers::RunMode;
 
-const MAX_SLICE_SIZE: usize = 4;
+const MAX_SLICE_SIZE_PART_1: usize = 4;
 
-fn find_last_repeated_char_location(string: &str, end_char_location: usize) -> Option<usize> {
-    for end_offset in 0..MAX_SLICE_SIZE {
+fn find_last_repeated_char_location(string: &str, end_char_location: usize, max_slice_size: usize) -> Option<usize> {
+    for end_offset in 0..(max_slice_size - 1) {
         // If we find the current last character in the given slice,
-            // Find the location in the slice, and return it.
+            // Return the later index as the new slice start
         // Otherwise, remove the last character and repeat, checking against the new last.
-        for offset in (end_offset + 1)..(MAX_SLICE_SIZE - end_offset) {
-            // println!("end_char_location={end_char_location}, offset={offset}, end_offset={end_offset}");
-            println!("Index {} vs {}", end_char_location - offset, end_char_location - end_offset);
-            println!("Comparing {:?} with {:?}", string.chars().nth(end_char_location - offset), string.chars().nth(end_char_location - end_offset));
+        for offset in (end_offset + 1)..max_slice_size {
             if string.chars().nth(end_char_location - offset) == string.chars().nth(end_char_location - end_offset) {
-                println!("Hit at {}!", end_char_location - end_offset);
-                return Option::from((end_char_location - end_offset));
-                // TODO: We're very close. We need to find the proper location to return once we've found a hit.
+                return Option::from((end_char_location - offset));
             }
         }
 
@@ -33,11 +28,10 @@ fn find_last_repeated_char_location(string: &str, end_char_location: usize) -> O
 pub fn part_1_solution(input_strings: Vec<String>, run_mode: RunMode) -> String {
     let master_string: &str = input_strings.last().unwrap();
 
-    let mut i = MAX_SLICE_SIZE - 1; // Check from the last character of the slice
+    let mut i = MAX_SLICE_SIZE_PART_1 - 1; // Check from the last character of the slice
     while i < master_string.len() {
-        println!("Checking at ending index={i}");
-        match find_last_repeated_char_location(master_string, i) {
-            Some(x) => i = x + (MAX_SLICE_SIZE - 1),
+        match find_last_repeated_char_location(master_string, i, MAX_SLICE_SIZE_PART_1) {
+            Some(x) => i = x + MAX_SLICE_SIZE_PART_1,
             None => return (i + 1).to_string(),
         }
     }
