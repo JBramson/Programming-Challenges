60650385e87b77a9b21c53c7a7177612ce610bb6
Jack Bramson <jackbramson@gmail.com>
2023-09-15 13:18:41 -0400

    Started gaining better understanding of day 7 part 1 solution.

--- AdventOfCode2022/src/day_7.rs
+++ AdventOfCode2022/src/day_7.rs
@@ -5,7 +5,6 @@
  * Part 2:
  * Part of me learning Rust.
  */
-use crate::day_7::CommandMode::{CHANGE_DIR, LIST_DIR};
 use crate::helpers::RunMode;
 use std::collections::HashMap;
 
@@ -16,33 +15,37 @@ struct Directory {
     size: i32, // 0 for dirs, non-zero for files
 }
 
+// TODO: make a function that recursively finds & assigns the size of each dir
+
 pub fn part_1_solution(input_strings: Vec<String>, run_mode: RunMode) -> String {
-    let mut directories: HashMap<&str, Directory> = HashMap::new();
-    let mut current_dir = "/"; // The root dir is its own parent
+    let mut directories: HashMap<String, Directory> = HashMap::new();
+    let mut current_dir: String = String::from("/"); // The root dir is its own parent
 
     // Hard-set the root dir, as it is its own parent
-    directories.insert(current_dir,  Directory{parent: String::from(current_dir), children: vec![], size: 0});
+    directories.insert(current_dir.clone(),  Directory{parent: current_dir.clone(), children: vec![], size: 0});
 
     for line in input_strings {
+        // Interpret commands
         if line.chars().nth(0).unwrap() == '$' {
             if &line[2..4] == "cd" {
                 if &line[5..] == ".." {
                     todo!("Move up 1 dir");
                 } else {
-                    current_dir = &line[5..];
+                    current_dir = String::from(&line[5..]);
                 }
             }
         } else {
             // Read in dir contents
-            //TODO: For BOTH, don't forget to add the new location's name as a child to the parent dir.
             if &line[0..3] == "dir" {
-                directories.insert(current_dir,  Directory{parent: String::from(current_dir), children: vec![], size: 0});
+                directories.insert(String::from(&line[3..]),  Directory{parent: current_dir.clone(), children: vec![], size: 0});
             } else {
                 //TODO: Split line input by the space and use the values to add the "dir".
             }
         }
         println!(" {line}");
     }
+
+    todo!("For each 'dir', add it (the string) to its parent's children vec.");
 
     String::from("Solution in-progress.")
 }
