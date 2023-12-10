60594c39e977f012ef9e27d02c4e0beb3743e9d5
Jack B <=>
2023-12-10 16:39:11 -0500

    Finished day 3 part 2.

--- aoc2023/src/day3.rs
+++ aoc2023/src/day3.rs
@@ -2,7 +2,7 @@
 * https://adventofcode.com/2023/day/3
 * Objective: Given a map of numbers and symbols,
 * Part 1: Find the sum of values bordering a symbol
-* Part 2: 
+* Part 2: Find the sum of products of values in places where exactly 2 values border an asterisk
 * Part of me learning Rust.
 */
 use crate::helpers::RunMode;
@@ -34,13 +34,6 @@ fn find_number_at_x(line: &mut String, x: usize, x_max: usize) -> i32 {
 
     value_string.parse::<i32>().unwrap()
 }
-
-/**
- * New idea:
- * Search for the symbols only.
- * If a symbol is detected, check its surroundings.
- *      From left to right, add the numbers starting from the left and replace the values with periods to avoid double-counting.
- */
 
 pub fn solve_part_1(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32, String> {
     let mut part_sums = 0;
@@ -99,11 +92,98 @@ pub fn solve_part_1(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32
 }
 
 pub fn solve_part_2(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32, String> {
-    let mut game_power_sets = 0;
+    let mut part_sums = 0;
+    let mut map = input_strings.to_owned();
+    let row_count = input_strings.len();
+    let col_count = input_strings.first().unwrap().len();
 
+    // Fill out the number and symbol vectors
+    for row_num in 0..row_count {
+        let check_up = row_num > 0;
+        let check_down = row_num < row_count - 1;
+        for col_num in 0..col_count {
+            let check_left = col_num > 0;
+            let check_right = col_num < col_count - 1;
+            match input_strings[row_num].chars().nth(col_num).unwrap() {
+                '0'..='9' | '.' => {},
+                _ => {
+                    let mut gear_ratio = 1;
+                    let mut gear_update_count = 0;
+                    let mut gear_update_val: i32;
+                    if check_up {
+                        // Check (y-1, x)
+                        gear_update_val = find_number_at_x(&mut map[row_num - 1], col_num, col_count);
+                        if gear_update_val != 0 {
+                            gear_ratio *= gear_update_val;
+                            gear_update_count += 1;
+                        }
+                        if check_left {
+                            // Check (y-1, x-1)
+                            gear_update_val = find_number_at_x(&mut map[row_num - 1], col_num - 1, col_count);
+                            if gear_update_val != 0 {
+                                gear_ratio *= gear_update_val;
+                                gear_update_count += 1;
+                            }
+                        }
+                        if check_right {
+                            // Check (y-1, x+1)
+                            gear_update_val = find_number_at_x(&mut map[row_num - 1], col_num + 1, col_count);
+                            if gear_update_val != 0 {
+                                gear_ratio *= gear_update_val;
+                                gear_update_count += 1;
+                            }
+                        }
+                    }
+                    if check_down {
+                        // Check (y+1, x)
+                        gear_update_val = find_number_at_x(&mut map[row_num + 1], col_num, col_count);
+                        if gear_update_val != 0 {
+                            gear_ratio *= gear_update_val;
+                            gear_update_count += 1;
+                        }
+                        if check_left {
+                            // Check (y+1, x-1)
+                            gear_update_val = find_number_at_x(&mut map[row_num + 1], col_num - 1, col_count);
+                            if gear_update_val != 0 {
+                                gear_ratio *= gear_update_val;
+                                gear_update_count += 1;
+                            }
+                        }
+                        if check_right {
+                            // Check (y+1, x+1)
+                            gear_update_val = find_number_at_x(&mut map[row_num + 1], col_num + 1, col_count);
+                            if gear_update_val != 0 {
+                                gear_ratio *= gear_update_val;
+                                gear_update_count += 1;
+                            }
+                        }
+                    }
+                    if check_left {
+                        // Check (y, x-1)
+                        gear_update_val = find_number_at_x(&mut map[row_num], col_num - 1, col_count);
+                        if gear_update_val != 0 {
+                            gear_ratio *= gear_update_val;
+                            gear_update_count += 1;
+                        }
+                    }
+                    if check_right {
+                        // Check (y, x+1)
+                        gear_update_val = find_number_at_x(&mut map[row_num], col_num + 1, col_count);
+                        if gear_update_val != 0 {
+                            gear_ratio *= gear_update_val;
+                            gear_update_count += 1;
+                        }
+                    }
+                    
+                    if gear_update_count == 2 {
+                        part_sums += gear_ratio;
+                    }
+                }, 
+            }
+        }
+    }
 
-
-    Ok(game_power_sets as i32)
+    Ok(part_sums as i32)
 }
 
 pub fn solve(input_strings: Vec<String>, run_mode: RunMode, puzzle_part: PuzzlePart) -> Result<i32, String> {
