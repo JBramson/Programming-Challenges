ae6b6588fe6b1144f36271974c0c34d2fef6466b
Jack B <=>
2023-12-31 20:56:01 -0500

    Finished day 4 part 2.

--- aoc2023/src/day4.rs
+++ aoc2023/src/day4.rs
@@ -2,7 +2,7 @@
 * https://adventofcode.com/2023/day/4
 * Objective: Given a list of scratch-off cards with winning numbers and our numbers,
 * Part 1: Find the number of points yielded from our winning numbers
-* Part 2: Find the total number of cards yielded if points grant us additional cards, given only the first for free
+* Part 2: Find the total number of cards acquired if points grant us additional cards, starting with one of each
 * Part of me learning Rust.
 */
 use crate::helpers::RunMode;
@@ -34,9 +34,8 @@ pub fn solve_part_1(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32
 
 // Idea: Create vector of ints that give the quantity of a given card unlocked, and keep going until we reach the end of the vec
 pub fn solve_part_2(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32, String> {
-    let mut card_count_total = 1;
-    let mut card_counts = vec![0; input_strings.len()];
-    card_counts[0] = 1; // First one's free
+    let mut card_count_total = 0;
+    let mut card_counts = vec![1; input_strings.len()];
     let mut current_card_index = 0;
 
     while current_card_index < card_counts.len() {
@@ -45,14 +44,22 @@ pub fn solve_part_2(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32
             break;
         }
         // Get the number of wins
+        let mut wins_on_card: usize = 0;
+        let winners: Vec<&str> = input_strings[current_card_index].split(" | ").nth(0).unwrap().split(" ").filter(|x| *x != "").collect();
+        let our_numbers: Vec<&str> =  input_strings[current_card_index].split(" | ").nth(1).unwrap().split(" ").filter(|x| *x != "").collect();
+        for winner in winners {
+            wins_on_card += our_numbers.iter().filter(|&x| *x == winner).count();
+        }
         // Use this number to calculate how many successive cards to add to
-        // Multiply this count by the current card's count and add this amount to each previously-determined card
+        for card_index in current_card_index+1..(current_card_index+wins_on_card+1) {
+            // Add this count to each previously-determined card
+            card_counts[card_index] += card_counts[current_card_index];
+        }
+
+        current_card_index += 1;
     }
 
     for card_count in card_counts {
-        if card_count == 0 {
-            break;
-        }
         card_count_total += card_count;
     }
     
