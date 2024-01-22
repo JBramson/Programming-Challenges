4c36056bcd85506814468608a3281d957dfdcf31
Jack B <=>
2024-01-21 19:23:10 -0500

    Broke ground on range-based part 2 solution, including range detection.

--- aoc2023/src/day5.rs
+++ aoc2023/src/day5.rs
@@ -17,6 +17,13 @@ fn get_offset_location(start_loc: u32, destination_range_start: u32, source_rang
     }
 }
 
+// Range is inclusive ([start, end])
+#[derive(Debug, PartialEq)]
+struct Range {
+    start: u32,
+    end: u32,
+}
+
 // Any unmapped sections correspond to their own number
 pub fn solve_part_1(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32, String> {
     let mut relevant_locations: Vec<u32> = input_strings[0].split(": ").nth(1).unwrap().split(" ").map(|x| x.parse::<u32>().unwrap()).collect();
@@ -56,12 +63,13 @@ pub fn solve_part_1(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32
 }
 
 pub fn solve_part_2(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32, String> {
-    let mut relevant_locations: Vec<u32> = input_strings[0].split(": ").nth(1).unwrap().split(" ").map(|x| x.parse::<u32>().unwrap()).collect();
-    
+    let starting_line: Vec<u32> = input_strings[0].split(": ").nth(1).unwrap().split(" ").map(|x| x.parse::<u32>().unwrap()).collect();
+
+    let mut relevant_ranges: Vec<Range> = vec![];
     let mut starts: Vec<u32> = vec![];
     let mut lengths: Vec<u32> = vec![];
 
-    for (index, value) in relevant_locations.iter().enumerate() {
+    for (index, value) in starting_line.iter().enumerate() {
         if index % 2 == 0 {
             starts.push(*value);
         } else {
@@ -69,18 +77,15 @@ pub fn solve_part_2(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32
         }
     }
 
-    relevant_locations.clear();
     for i in 0..starts.len() {
-        relevant_locations.append(&mut (starts[i]..(starts[i]+lengths[i])).collect());
+        let new_range = Range{start: starts[i], end: starts[i] + lengths[i] - 1};
+        relevant_ranges.push(new_range);
+
     }
 
+    println!("current_ranges.len()={}", relevant_ranges.len());
+    println!("current_ranges={:?}", relevant_ranges);
     
-    relevant_locations.sort();
-    println!("relevant_locations.len()={}", relevant_locations.len());
-    relevant_locations.dedup();
-    println!("relevant_locations.len() post-dedup()={}", relevant_locations.len());
-    
-    return Err(String::from("Still working on it."));
 
     /*
      * TODO: This is going to have to go by ranges and holes. We'll start with a range of values for the seeds with no holes.
@@ -88,32 +93,33 @@ pub fn solve_part_2(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32
      * If there are, add a new, shifted range of the updated values and add a hole range with the old values (AFTER ALL CHECKS OF THE ROUND).
      * During future checks, delete hits from hole ranges (if 10-19 is a hole, 1-30 becomes 1-9 and 20-30).
      */
-    
-    // let mut new_locations: Vec<u32> = vec![];
-    // let mut changed_locations: Vec<u32> = vec![];
 
-    // for line in &input_strings[3..] {
-    //     if line.is_empty() {
-    //         continue;
-    //     } else if line.contains(":") {
-    //         relevant_locations.retain(|x| !changed_locations.contains(x));
-    //         relevant_locations.append(&mut new_locations);
-    //         changed_locations.clear();
-    //         new_locations.clear();
-    //         continue;
-    //     }
+    let mut new_ranges: Vec<Range> = vec![];
+
+    for line in &input_strings[3..] {
+        if line.is_empty() {
+            continue;
+        } else if line.contains(":") {
+            relevant_ranges.append(&mut new_ranges);
+            new_ranges.clear();
+            continue;
+        }
         
-    //     let almanac_entry: Vec<u32> = line.split(" ").map(|x| x.parse::<u32>().unwrap()).collect();
-    //     for relevant_location in &relevant_locations {
-    //         match get_offset_location(*relevant_location, almanac_entry[0], almanac_entry[1], almanac_entry[2]) {
-    //             Some(new_location) => {
-    //                 changed_locations.push(*relevant_location);
-    //                 new_locations.push(new_location);
-    //             },
-    //             _ => {},
-    //         }
-    //     }
-    // }
+        let almanac_entry: Vec<u32> = line.split(" ").map(|x| x.parse::<u32>().unwrap()).collect();
+        for relevant_range in &relevant_ranges {
+            if relevant_range.start >= almanac_entry[1] && relevant_range.start <= almanac_entry[1] + almanac_entry[2] - 1 {
+                println!("Hit detected, starting at {}, in {:?}", relevant_range.start, almanac_entry);
+            }
+            
+            // match get_offset_location(*relevant_location, almanac_entry[0], almanac_entry[1], almanac_entry[2]) {
+            //     Some(new_location) => {
+            //         // changed_locations.push(*relevant_location);
+            //         new_locations.push(new_location);
+            //     },
+            //     _ => {},
+            // }
+        }
+    }
 
     // // Run the cleanup stuff afterwards due to lack of ending colon line
     // relevant_locations.retain(|x| !changed_locations.contains(x));
@@ -122,6 +128,8 @@ pub fn solve_part_2(input_strings: Vec<String>, run_mode: RunMode) -> Result<i32
     // new_locations.clear();
 
     // Ok(*relevant_locations.iter().min().unwrap() as i32)
+
+    return Err(String::from("Still working on it."));
 }
 
 pub fn solve(input_strings: Vec<String>, run_mode: RunMode, puzzle_part: PuzzlePart) -> Result<i32, String> {
