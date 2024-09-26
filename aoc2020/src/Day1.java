f7cb8ea3f7f7c5b5b0ffacb05503a7e0db69c708
Jack Bramson <jackbramson@gmail.com>
2024-09-25 23:45:43 -0700

    Created framework for all days' solutions.

---
+++ aoc2020/src/Day1.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class Day1 {
+    boolean inDebugMode;
+    boolean isDayOne;
+    List<String> inputStrings;
+
+    Day1(boolean inDebugMode, boolean isDayOne, List<String> inputStrings) {
+        this.inDebugMode = inDebugMode;
+        this.isDayOne = isDayOne;
+        this.inputStrings = inputStrings;
+    }
+
+    private int solve_part_1() {
+
+
+        return -1;
+    }
+
+    private int solve_part_2() {
+
+
+        return -1;
+    }
+
+    public int solve() {
+        return isDayOne ? solve_part_1() : solve_part_2();
+    }
+}
--- aoc2020/src/Main.java
+++ aoc2020/src/Main.java
@@ -1,9 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello, World!");
+    public static final boolean IS_DEBUG = true;
+    public static final boolean IS_DAY_1 = true;
+
+    public static void main(String[] args) throws IOException {
+        String inputFileStr = IS_DEBUG ? "input/debug_input.txt" : "input/deployment_input.txt";
+        File inputFile = new File(inputFileStr);
+        inputFile.getParentFile().mkdirs();
+        try {
+            if (inputFile.createNewFile()) {
+                System.out.println("Input file " + inputFile + " created. Please put the input in there and restart.");
+                return;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scanner sc = new Scanner(inputFile);
+        List<String> inputLines = new ArrayList<String>();
+        while (sc.hasNextLine()) {
+            inputLines.add(sc.nextLine());
+        }
+
+        Day1 solution = new Day1(IS_DEBUG, IS_DAY_1, inputLines);
+        System.out.println("Result = " + solution.solve());
     }
 }
