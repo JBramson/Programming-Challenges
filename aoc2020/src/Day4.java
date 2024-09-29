3a8b28d1e8080b8a03f5dbd384b6e76b17937121
Jack Bramson <jackbramson@gmail.com>
2024-09-28 23:10:18 -0700

    Laid out rough draft for Day 4 part 2. Fails valid passports- needs resolution.

--- aoc2020/src/Day4.java
+++ aoc2020/src/Day4.java
@@ -4,6 +4,7 @@
  * Part 1: Find the number missing no more than the "cid" field.
  * Part 2:
  */
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -19,8 +20,56 @@ public class Day4 {
     }
 
     private boolean isValidPassport(final HashMap<String, String> passport, final String[] requiredFields) {
+        String[] VALID_EYE_COLORS = {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
         for (String requiredField : requiredFields) {
             if (!passport.containsKey(requiredField)) return false;
+            /**
+             * NOTE: There is no difference between solvePart1() and solvePart2()-
+             * the only difference is in this function, checking the given values for each key.
+             */
+            if (!isDayOne) {
+                try {
+                    switch (requiredField) {
+                        case "byr":
+                            final int birthYear = Integer.parseInt(passport.get("byr"));
+                            if (birthYear < 1920 || birthYear > 2002) return false;
+                            break;
+                        case "iyr":
+                            final int issueYear = Integer.parseInt(passport.get("iyr"));
+                            if (issueYear < 2010 || issueYear > 2020) return false;
+                            break;
+                        case "eyr":
+                            final int expirationYear = Integer.parseInt(passport.get("eyr"));
+                            if (expirationYear < 2020 || expirationYear > 2030) return false;
+                            break;
+                        case "hgt":
+                            final int height = Integer.parseInt(passport.get("hgt").substring(0, passport.get("hgt").length() - 3));
+                            if (passport.get("hgt").contains("cm")) {
+                                if (height < 150 || height > 193) return false;
+                            } else {
+                                if (height < 59 || height > 76) return false;
+                            }
+                        case "hcl":
+                            final String hairColor = passport.get("hcl");
+                            if (hairColor.charAt(0) != '#' || hairColor.length() != 7) return false;
+
+                            for (char c : hairColor.substring(1, hairColor.length() - 1).toCharArray()) {
+                                if (!Character.isLetterOrDigit(c)) return false;
+                            }
+                            break;
+                        case "ecl":
+                            final String eyeColor = passport.get("ecl");
+                            if (!Arrays.asList(VALID_EYE_COLORS).contains(eyeColor)) return false;
+                            break;
+                        case "pid":
+                            final String passportID = passport.get("pid");
+                            Integer.parseInt(passportID); // Check if numeric. Can throw NumberFormatException
+                            if (passportID.length() != 9) return false;
+                    }
+                } catch(NumberFormatException e){
+                    return false; // This should happen if someone tries to pass a letter as a number.
+                }
+            }
         }
 
         return true;
@@ -28,13 +77,13 @@ public class Day4 {
 
     private int solvePart1() {
         int validPassportCount = 0;
-        String[] mandatoryFields = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
+        final String[] MANDATORY_FIELDS = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
 
         HashMap<String, String> passport = new HashMap<>();
         for (String line : inputStrings) {
             // See if we're at the end of a data batch
             if (line.isEmpty()) {
-                if (isValidPassport(passport, mandatoryFields)) validPassportCount++;
+                if (isValidPassport(passport, MANDATORY_FIELDS)) validPassportCount++;
                 passport.clear();
             } else {
                 // Otherwise, split line by spaces, then for each, split it by ":", and add halves to the passport.
@@ -49,10 +98,25 @@ public class Day4 {
     }
 
     private int solvePart2() {
-        int validPassports = 0;
+        int validPassportCount = 0;
+        final String[] MANDATORY_FIELDS = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
 
+        HashMap<String, String> passport = new HashMap<>();
+        for (String line : inputStrings) {
+            // See if we're at the end of a data batch
+            if (line.isEmpty()) {
+                if (isValidPassport(passport, MANDATORY_FIELDS)) validPassportCount++;
+                passport.clear();
+            } else {
+                // Otherwise, split line by spaces, then for each, split it by ":", and add halves to the passport.
+                for (String field : line.split(" ")) {
+                    String[] fieldSplit = field.split(":");
+                    passport.put(fieldSplit[0], fieldSplit[1]);
+                }
+            }
+        }
 
-        return validPassports;
+        return validPassportCount;
     }
 
     public int solve() {
