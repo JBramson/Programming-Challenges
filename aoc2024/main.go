618d17105d5232871f47cc76eddaa647a9102175
Jack Bramson <jackbramson@gmail.com>
2026-06-20 22:41:04 -0600

    Set up base for AOC 2024 solutions.

---
+++ aoc2024/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	InputDir        = "input/"
+	ExampleInput    = "exampleInput"
+	PuzzleInput     = "puzzleInput"
+	ExampleSolution = "exampleSolution"
+)
+
+var files = [...]string{ExampleInput, PuzzleInput, ExampleSolution}
+
+// Checks to see if input files are set up. Will attempt to create dir/empty files if they don't exist.
+// Returns true if they're already ready and false if they aren't. Also returns a string of the files dir.
+func handleFileSetup() (bool, string) {
+	fileStatusIsPerfect := true
+	inputFolderFullPath, _ := os.Getwd()
+	// Create dir if not present
+	if _, err := os.Stat(InputDir); err != nil {
+		fileStatusIsPerfect = false
+		os.Mkdir(InputDir, os.ModePerm)
+	}
+	// Create files if not present
+	for _, fileStr := range files {
+		if _, err := os.Stat(InputDir + fileStr); err != nil {
+			fileStatusIsPerfect = false
+			_, err = os.Create(InputDir + fileStr)
+			if err != nil {
+				log.Fatal("Could not get full input folder dir. Error:", err)
+			}
+		}
+	}
+
+	return fileStatusIsPerfect, inputFolderFullPath
+}
+
+func getInput(inputFileStr string) []string {
+	rawFileContent, err := os.ReadFile(InputDir + inputFileStr)
+	if err != nil {
+		log.Fatal("Couldn't read ", inputFileStr, "err:", err)
+	}
+
+	return strings.Split(string(rawFileContent), "\n")
+}
+
+func main() {
+	inputsReady, inputDirLocation := handleFileSetup() // Set up files first
+	if !inputsReady {
+		fmt.Println("Blank files have been created at", inputDirLocation)
+		return
+	}
+
+	exampleSolutionInt, err := strconv.Atoi(getInput(ExampleSolution)[0])
+	if err != nil {
+		log.Fatal("Couldn't convert example solution to int. err:", err)
+	}
+
+	fmt.Println(exampleSolutionInt)
+
+}
