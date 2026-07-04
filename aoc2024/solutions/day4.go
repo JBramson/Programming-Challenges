d4832bb588a4d87da3851e45a4a25e1536fcd6a2
Jack Bramson <jackbramson@gmail.com>
2026-07-04 12:57:50 -0600

    Made progress on Day 4 Part 2.

--- aoc2024/solutions/day4.go
+++ aoc2024/solutions/day4.go
@@ -2,7 +2,7 @@
  * https://adventofcode.com/2024/day/4
  * Objective: Given a grid of letters,
  * Part 1: Find the total number of times "XMAS" appears, be it "horizontal, vertical, diagonal, written backwards, or even overlapping other words."
- * Part 2:
+ * Part 2: Find the total number of times "MAS" appears in an "X" shape.
  * Part of me learning Go.
  */
 package solutions
@@ -16,8 +16,8 @@ import (
  * Declare array of 8 bools representing directions to check. Positions start at North, rotating 45 degrees clockwise with each entry.
  * Too high, don't check North (7, 0, 1)
  * Too low, don't check South (3, 4, 5)
- * Too left, don't check East (5, 6, 7)
- * Too right, don't check West (1, 2, 3)
+ * Too left, don't check West (5, 6, 7)
+ * Too right, don't check East (1, 2, 3)
  */
 func getSearchDirections(tooHigh bool, tooLow bool, tooLeft bool, tooRight bool) []bool {
 	directionChecklist := make([]bool, 8)
@@ -136,6 +136,80 @@ func solveDay4P1(lines []string) int {
 }
 func solveDay4P2(lines []string) int {
 	totalXmases := 0
+	grid := make([][]rune, len(lines))
+	xmasSlice := []rune{'M', 'A', 'S'}
+	height := len(lines)
+	length := len(lines[0])
+	// Array of 4 bools representing directions to check. Positions start at North-East, rotating 90 degrees clockwise with each entry.
+	directionChecklist := make([]bool, 4)
+	for lineNum, line := range lines {
+		// Add line to grid (must be done before checks)
+		grid[lineNum] = []rune(line)
+	}
+
+	// For each new column, check against the length and determine if we should be checking up/down.
+	for lineNum, line := range lines {
+		// Directions are checked by default and falsified if the range is insufficient.
+		for i := range len(directionChecklist) {
+			directionChecklist[i] = true
+		}
+		// Vertical directions are checked once per line.
+		if lineNum == 0 || lineNum == height-1 {
+			continue
+		}
+		for letterNum, letter := range line {
+			if letter != xmasSlice[1] {
+				// Skip non-`A`s.
+				continue
+			}
+			// Horizontal directions are checked once per letter.
+			if letterNum == 0 || letterNum == length-1 {
+				continue
+			}
+
+			fmt.Println("Checking line ", lineNum, " letter ", letterNum, " for XMAS.")
+
+			// Check forward-slash side of the X.
+			if grid[lineNum-1][letterNum+1] == xmasSlice[0] {
+				// Upper-Right is `M`
+				if grid[lineNum+1][letterNum+1] != xmasSlice[2] {
+					// Bottom-right is NOT `S`
+					continue
+				}
+			} else if grid[lineNum-1][letterNum+1] == xmasSlice[2] {
+				// Upper-Right is `S`
+				if grid[lineNum+1][letterNum+1] != xmasSlice[0] {
+					// Bottom-right is NOT `M`
+					continue
+				}
+			} else {
+				// Upper-Right is neither `M` nor `S`
+				continue
+			}
+
+			// Check back-slash side of the X.
+			if grid[lineNum-1][letterNum-1] == xmasSlice[0] {
+				// Upper-Left is `M`
+				if grid[lineNum+1][letterNum-1] != xmasSlice[2] {
+					// Bottom-left is NOT `S`
+					continue
+				}
+			} else if grid[lineNum-1][letterNum-1] == xmasSlice[2] {
+				// Upper-Left is `S`
+				if grid[lineNum+1][letterNum-1] != xmasSlice[0] {
+					// Bottom-left is NOT `M`
+					continue
+				}
+			} else {
+				// Upper-Left is neither `M` nor `S`
+				continue
+			}
+
+			totalXmases++
+		}
+
+	}
+
 	return totalXmases
 }
 
